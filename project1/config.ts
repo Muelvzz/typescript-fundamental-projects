bcb92d49dfc68d27d4b41e62bb37d8c0f60b7360
Muelvin Lopez <muelvinlopez25@gmail.com>
2026-03-31 07:55:21 +0800

    startup

---
+++ project1/config.ts
@@ -0,0 +1,7 @@
+export type userContact = {
+    firstName: string
+    email: string
+    number: number    
+}
+
+export let contacts = []
---
+++ project1/startup.ts
@@ -0,0 +1,3 @@
+export function startup() {
+    console.log("--- WELCOME TO CONTACT DIRECTORY 📱 ---")
+}
