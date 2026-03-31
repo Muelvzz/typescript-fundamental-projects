3252a197d241247e540c06f09923558dc2544c7e
Muelvin Lopez <muelvinlopez25@gmail.com>
2026-03-31 08:02:50 +0800

    Call for Action

--- project1/startup.ts
+++ project1/startup.ts
@@ -1,3 +1,8 @@
 export function startup() {
     console.log("--- WELCOME TO CONTACT DIRECTORY 📱 ---")
+
+    console.log("\n--- COMMANDS 🎮 ---")
+    console.log("1. Add Contact")
+    console.log("2. View All Contacts")
+    console.log("3. Exit")
 }
