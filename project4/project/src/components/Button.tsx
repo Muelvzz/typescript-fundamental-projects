6e81297e280dc78db072dcc4fb08c61c31a7c3bc
Muelvin Lopez <muelvinlopez25@gmail.com>
2026-04-06 21:39:09 +0800

    Calculator Frontend

--- project4/project/src/components/Button.tsx
+++ project4/project/src/components/Button.tsx
@@ -1,7 +1,20 @@
 import { JSX } from "react";
 
-export default function Button(): JSX.Element {
+// type buttonProps = { value: string, index: number }
+
+export default function Button({ value }: { value: string }): JSX.Element {
     return (
-        <div></div>
+        <button className={`
+            text-white bg-[#17191c] p-5 w-full h-full 
+            font-bold rounded-lg cursor-pointer text-lg
+            ${ value === '=' ? 'bg-red-500' : "" }
+            ${ value === 'DEL' || value === 'AC' ? 'bg-green-400' : '' } 
+            ${ value === 'X' || value === '/' || 
+                value === '+' || value === '-' 
+                ? 'bg-amber-300' : '' } 
+            hover:bg-[#17191cab] duration-300
+            `}
+            key={ value }
+            >{ value }</button>
     )
 }
--- project4/project/src/components/Calculator.tsx
+++ project4/project/src/components/Calculator.tsx
@@ -1,9 +1,32 @@
 import { JSX } from "react";
+import Button from "./Button";
 
 export default function Calculator(): JSX.Element {
+
+    const keyList: string[] = ['7', '8', '9', 'DEL', 'AC', '4', '5', '6', 'X', '/', '1', '2', '3', '+', '-', '0', '.', 'x10^x', '=']
+
     return (
-        <section>
-            <h1>This is the Calculator Component</h1>
+        <section className="
+            flex flex-col gap-y-2 bg-black px-10 pb-10 pt-20 rounded-lg
+        ">
+            <div className="
+                bg-white text-right pb-2 pr-4 pt-10 rounded-lg
+            ">
+                <h1 className="
+                    font-semibold text-4xl
+                ">Answer</h1>
+            </div>
+            <div className="
+                grid grid-cols-5 gap-2
+            ">
+                { keyList.map((value: string): JSX.Element => (
+                    <div>
+                        <Button 
+                            value={ value }
+                        />
+                    </div>
+                )) }
+            </div>
         </section>
     )
 }
